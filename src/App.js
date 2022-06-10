8b8fdbd98e30aad64e8cf57c147e8d2f226f5267
Dimitri ALMON <dalm1@MacBook-Air-de-Dimitri.local>
2022-06-10 05:30:47 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Photo from "./galleri.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="maintitl">
+        <p>In development</p>
+      </div>
+
+      <>
+        <p></p>
+      </>
+      <div className="logo">Ω</div>
+      <img src={Photo} alt="" />
     </div>
   );
 }
